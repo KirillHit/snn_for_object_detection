ed2bf031548e14abe6ad184a08f8f91e22dde7cc
KirillHit <khitushkink@mail.ru>
2024-07-28 22:30:45 +0300

    Update startup file

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import utils
 import engine
 import models
-import torch
-from torch import nn
-import numpy as np
 from matplotlib import pyplot as plt
 import torchvision
-import time
 from torchviz import make_dot
 
 import utils.devices
+
+def ask_question(question, default="y"):
+    valid = {"y": True, "n":False}
+    prompt = " [y/n] "
+
+    while True:
+        print(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'y' or 'n'")
 
 if __name__ == "__main__":
     transform = torchvision.transforms.Compose(
@@ -18,19 +28,21 @@ if __name__ == "__main__":
             torchvision.transforms.Normalize((0.23, 0.23, 0.23), (0.12, 0.12, 0.12))
         ]
     )
-    data = utils.BananasDataset(batch_size=16, transform=transform)
-
-    model = models.SpikeYOLO(num_classes=1)
+    data = utils.BananasDataset(batch_size=32, transform=transform)
+    model = models.YOLO(num_classes=1)
     model.to(utils.devices.gpu())
-    #make_dot(model(torch.unsqueeze(data[0][0].to("cuda"), dim=0)), params=dict(model.named_parameters())).render("rnn_torchviz2", format="png")
-    model.load_params()
-
-    trainer = engine.Trainer(max_epochs=0, num_gpus=1, display=True, every_n=1)
-    trainer.fit(model, data)
-    #model.save_params()
-    
+    trainer = engine.Trainer(max_epochs=1, num_gpus=1, display=True, every_n=1)
     plotter = utils.Plotter(threshold=0.001, rows=2, columns=4, labels=data.get_names())
-    trainer.test_model(data, plotter, is_train=False)
+    
+    #make_dot(model(torch.unsqueeze(data[0][0].to("cuda"), dim=0)), params=dict(model.named_parameters())).render("rnn_torchviz2", format="png")
+    
+    if (ask_question("Load parameters?")):
+        model.load_params()
 
-    plt.ioff()
-    plt.show()
+    while(ask_question("Start fit?")):
+        trainer.fit(model, data)
+        trainer.test_model(data, plotter, is_train=False)
+        plt.show()
+
+    if (ask_question("Save parameters?")):
+        model.save_params()
