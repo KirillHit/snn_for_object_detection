d29f51733556ac0d2776c7ee944f281473ce8ae1
KirillHit <khitushkink@mail.ru>
2024-07-31 22:45:39 +0300

    add bananas.params

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from .datasets import BananasDataset, Cifar10Dataset, ImagenetteDataset, HardHatDataset
+from .datasets import BananasDataset, HardHatDataset
 from .plots import ProgressBoard
 from .plotter import Plotter
 
-__all__ = "BananasDataset", "ProgressBoard", "Plotter", "Cifar10Dataset", "ImagenetteDataset", "HardHatDataset"
+__all__ = "BananasDataset", "ProgressBoard", "Plotter", "HardHatDataset"
