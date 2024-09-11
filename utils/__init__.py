9e0ed3a941238785a06e0e6c9940af0e77247dab
KirillHit <khitushkink@mail.ru>
2024-09-11 19:25:58 +0300

    add gen1 dataset

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
-from .datasets import BananasDataset, HardHatDataset
+from .datasets import Gen1Dataset
 from .plots import ProgressBoard
 from .plotter import Plotter
 
-__all__ = "BananasDataset", "ProgressBoard", "Plotter", "HardHatDataset"
+__all__ = "ProgressBoard", "Plotter", "Gen1Dataset"
