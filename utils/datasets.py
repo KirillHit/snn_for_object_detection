a0dffc946ba8ca59bfd07dceec23ffcce4c062e3
KirillHit <khitushkink@mail.ru>
2024-09-30 14:10:28 +0300

    add Gen1FixedDataset

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,27 +1,28 @@
 import glob
 import os
-import xml.etree.ElementTree as ET
-
 import numpy as np
-import pandas as pd
-import PIL
-import PIL.Image
 import torch
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
-from tqdm import tqdm
-
+from torchvision.transforms.v2._geometry import Resize
+from torchvision.transforms.v2._misc import Normalize
 from engine.data import DataModule
-from utils.downloads import download_extract
-
 from .io.psee_loader import PSEELoader
 
 
-class Gen1Dataset(DataModule):
+class Gen1DataModule(DataModule):
     """Event-Based Dataset to date"""
 
-    duration: int = 1000  # ms
-    time_step: int = 1  # ms
+    def __init__(
+        self,
+        root="./data",
+        num_workers=4,
+        batch_size=32,
+        resize: Resize = None,
+        normalize: Normalize = None,
+        duration=10000,
+    ):
+        super().__init__(root, num_workers, batch_size, resize, normalize)
+        self.duration = duration
 
     def read_data(self, split: str):
         # Data dir: ./data/gen1/<test, train, val>
@@ -31,182 +32,146 @@ class Gen1Dataset(DataModule):
         # Get files name
         gt_files = glob.glob(data_dir + "/*_bbox.npy")
         data_files = [p.replace("_bbox.npy", "_td.dat") for p in gt_files]
-        # npy format: ('ts [us]', 'x', 'y', 'w', 'h', 'class_id', 'confidence', 'track_id')
-        # class_id 0 for cars and 1 for pedestrians
-        # boxes are updated once per second       
-        # TODO
-        np_gt_boxes = np.ndarray([np.load(p) for p in gt_files])
-        m = np_gt_boxes[:][:]["ts"]
-        
-        self.events_loaders = [PSEELoader(td_file) for td_file in data_files]
-        features = self.parse_features()
 
-    def parse_features(self):
-        """Transforms events into a video stream"""
-        # [dur, batch, c [0-negative, 1-positive], h, w]
-        height, width = self.events_loaders[0].get_size()
-        features = torch.zeros(
-            [self.duration, self.batch_size, 2, height, width],
-            dtype=torch.float32,
-        )
-        # [self.duration, self.width, self.height, 2],
-        for batch_num in range(self.batch_size):
-            # Events format ('t' [us], 'x', 'y', 'p' [1-positive/0-negative])
-            events = self.events_loaders[batch_num].load_delta_t(self.duration * 1000)
-            time_stamp = events[0]["t"] // (self.time_step * 1000)
-            events[:]["t"] = (events[:]["t"] // (self.time_step * 1000)) - time_stamp
-            mask = np.stack(
-                [events[:]["t"], events[:]["p"], events[:]["y"], events[:]["x"]],
-                axis=1,
-                dtype=np.int32,
+        if not data_files or not gt_files:
+            raise RuntimeError(
+                f"Directory '{data_dir}' does not contain data! I'm expecting: ./data/{data_dir}/*_bbox.npy (and *_td.dat). The dataset can be downloaded from this link: https://www.prophesee.ai/2020/01/24/prophesee-gen1-automotive-detection-dataset/"
             )
-            features[mask[:, 0], batch_num, mask[:, 1], mask[:, 2], mask[:, 3]] = 1
-            
-            # labels format: class id (0 car, 1 person), xmin, ymin, xmax, ymax
-            gt_boxes = self.gt_boxes_list[batch_num]
-            gt_boxes[:]["ts"] = gt_boxes[:]["ts"] // (self.time_step * 1000)
-            _, counts = np.unique(gt_boxes[:]["ts"], return_counts=True)
-            labels_count = np.max(counts)
-            torch.full((), -1, dtype=torch.float32)
-            gt_boxes
-            
-        return features
 
-    def parse_targets(self):
+        gt_boxes_list = [np.load(p) for p in gt_files]
+        events_loaders = [PSEELoader(td_file) for td_file in data_files]
+
+        dataset = self.create_dataset((events_loaders, gt_boxes_list))
+
+        match split:
+            case "train":
+                self._train_dataset = dataset
+            case "test":
+                self._test_dataset = dataset
+            case "val":
+                self._val_dataset = dataset
+
+        print(
+            str(len(events_loaders))
+            + " "
+            + split
+            + " samples loaded from Gen1 dataset..."
+        )
+
+    def create_dataset(self, data: list):
+        raise NotImplementedError
+
+    def get_labels(self):
         names = ("car", "person")
         return names
 
 
-class CustomDataset(Dataset):
+class Gen1Fixed(Gen1DataModule):
+    """Returns a sequence of event histograms with a fixed time step"""
+
+    def __init__(
+        self,
+        root="./data",
+        num_workers=4,
+        batch_size=32,
+        resize: Resize = None,
+        normalize: Normalize = None,
+        duration=10000,
+        time_step=100,
+    ):
+        super().__init__(root, num_workers, batch_size, resize, normalize, duration)
+        self.time_step = time_step
+
+    def create_dataset(self, data: list):
+        events_loaders, gt_boxes_list = data
+
+        for gt_boxes in gt_boxes_list:
+            gt_boxes[:]["ts"] //= self.time_step * 1000
+
+        return Gen1FixedDataset(
+            (events_loaders, gt_boxes_list),
+            self.time_step,
+            self.duration,
+        )
+
+
+class Gen1FixedDataset(Dataset):
     """A customized dataset to load the banana detection dataset."""
 
-    def __init__(self, data: list):
-        self.features, self.labels = data
+    record_time = 60000  # ms
+
+    def __init__(self, data: list, time_step, duration):
+        self.events_loaders, self.gt_boxes_list = data
+        self.duration, self.time_step = duration, time_step
+        self.record_steps = ((self.record_time - 1) // self.duration) + 1
 
     def __getitem__(self, idx):
-        return (self.features[idx].float(), self.labels[idx])
+        return self.parse_data(idx)
 
     def __len__(self):
-        return len(self.features)
+        return len(self.events_loaders) * self.record_steps
 
+    def parse_data(self, idx):
+        """Transforms events into a video stream"""
+        data_idx = idx // self.record_steps
+        height, width = self.events_loaders[0].get_size()
 
-class HardHatDataset(DataModule):
-    """Dataset for identifying hard hat"""
-
-    def read_data(self, is_train=True):
-        data_dir = download_extract("Hardhat", folder=self._root)
-        data_dir = os.path.join(data_dir, "Train" if is_train else "Test")
-
-        targets_path = os.path.join(data_dir, "targets.pt")
-        images_path = os.path.join(data_dir, "images.pt")
-
-        if os.path.exists(targets_path) and os.path.exists(images_path):
-            targets = torch.load(targets_path)
-            images = torch.load(images_path)
-            print("Tensors load")
-        else:
-            images, targets = self.parse(data_dir)
-            if self.save_tensor:
-                torch.save(targets, targets_path)
-                torch.save(images, images_path)
-
-        if is_train:
-            self._train_dataset = CustomDataset((images, targets))
-        else:
-            self._val_dataset = CustomDataset((images, targets))
-
-    def parse(self, data_dir):
-        labels_dir = os.path.join(data_dir, "Annotation")
-        images_dir = os.path.join(data_dir, "JPEGImage")
-
-        images, targets = [], []
-        for xml_file in os.listdir(labels_dir):
-            tree = ET.parse(os.path.join(labels_dir, xml_file))
-            root = tree.getroot()
-            size = root.find("size")
-            if size.find("depth").text != "3":
-                print("Warning: wrong depth")
-                continue
-            w = float(size.find("width").text)
-            h = float(size.find("height").text)
-            labels = []
-            for obj in root.findall("object"):
-                name = obj.find("name").text
-                classes = ("helmet",)  # "head", "person"
-                if name not in classes:
-                    # print("Warning: wrong name - " + name + " " + xml_file)
-                    continue
-                coord = obj.find("bndbox")
-                labels.append(
-                    (
-                        classes.index(name),
-                        float(coord.find("xmin").text) / w,
-                        float(coord.find("ymin").text) / h,
-                        float(coord.find("xmax").text) / w,
-                        float(coord.find("ymax").text) / h,
-                    )
-                )
-            if not labels:
-                continue
-            targets.append(labels)
-            filename = root.find("filename").text
-            img = PIL.Image.open(os.path.join(images_dir, filename))
-            images.append(self.transform(img))
-        targets = pad_sequence(
-            [torch.tensor(target) for target in targets],
-            batch_first=True,
-            padding_value=-1,
+        ############ Features preparing ############
+        # Features format (ts, c [0-negative, 1-positive], h, w)
+        features = torch.zeros(
+            [self.duration, 2, height, width],
+            dtype=torch.float32,
         )
+        # Events format ('t' [us], 'x', 'y', 'p' [1-positive/0-negative])
+        events = self.events_loaders[data_idx].load_delta_t(self.duration * 1000)
+        time_stamps = events[:]["t"] // (self.time_step * 1000)
+        features[
+            time_stamps[:] - time_stamps[0],
+            events[:]["p"].astype(np.uint32),
+            events[:]["y"].astype(np.uint32),
+            events[:]["x"].astype(np.uint32),
+        ] = 1
 
-        return torch.stack(images), targets
+        ############ Labels preparing ############
+        # npy format ('ts [us]', 'x', 'y', 'w', 'h', 'class_id', 'confidence', 'track_id')
+        # Box update frequency 1-4 Hz
+        gt_boxes = self.gt_boxes_list[data_idx]
+        gt_boxes_masked = gt_boxes[
+            (gt_boxes[:]["ts"] >= time_stamps[0])
+            & (gt_boxes[:]["ts"] <= time_stamps[-1])
+        ]
+        # Labels format (ts, class id (0 car, 1 person), xlu, ylu, xrd, yrd)
+        labels = torch.tensor(
+            [
+                gt_boxes_masked[:]["ts"],
+                gt_boxes_masked[:]["class_id"],
+                gt_boxes_masked[:]["x"],
+                gt_boxes_masked[:]["y"],
+                gt_boxes_masked[:]["x"] + gt_boxes_masked[:]["w"],
+                gt_boxes_masked[:]["y"] + gt_boxes_masked[:]["h"],
+            ],
+            dtype=torch.float32,
+        ).t()
 
-    def get_labels(self):
-        names = ("helmet",)
-        return names
+        return (features, labels)
 
 
-class BananasDataset(DataModule):
-    """A customized dataset to load the banana detection dataset."""
+class Gen1Adaptive(Gen1DataModule):
+    """Returns a sequence of event histograms with an adaptive time step"""
 
-    def read_data(self, is_train=True):
-        data_dir = download_extract("banana-detection", folder=self._root)
-        csv_fname = os.path.join(
-            data_dir, "bananas_train" if is_train else "bananas_val", "label.csv"
-        )
-        csv_data = pd.read_csv(csv_fname)
-        csv_data = csv_data.set_index("img_name")
-        images, targets = [], []
-        for img_name, target in csv_data.iterrows():
-            images.append(
-                self.transform(
-                    PIL.Image.open(
-                        os.path.join(
-                            data_dir,
-                            "bananas_train" if is_train else "bananas_val",
-                            "images",
-                            f"{img_name}",
-                        )
-                    )
-                )
-            )
-            targets.append(list(target))
-            # Here `target` contains (class, upper-left x, upper-left y,
-            # lower-right x, lower-right y), where all the images have the same
-            # banana class (index 0)
-        if is_train:
-            self._train_dataset = CustomDataset(
-                (images, torch.tensor(targets).unsqueeze(1) / 256)
-            )
-        else:
-            self._val_dataset = CustomDataset(
-                (images, torch.tensor(targets).unsqueeze(1) / 256)
-            )
-        print(
-            "Read "
-            + str(len(targets))
-            + (" training examples" if is_train else " validation examples")
-        )
+    def __init__(
+        self,
+        root="./data",
+        num_workers=4,
+        batch_size=32,
+        resize: Resize = None,
+        normalize: Normalize = None,
+        duration=10000,
+        event_step=100,
+    ):
+        super().__init__(root, num_workers, batch_size, resize, normalize, duration)
+        self.event_step = event_step
 
-    def get_labels(self):
-        names = ("bananas",)
-        return names
+    def create_dataset(self, data: list):
+        # TODO
+        raise NotImplementedError
