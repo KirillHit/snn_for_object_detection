a4a35dfe715e1cd149873d10209f3bf5b0bda147
KirillHit <khitushkink@mail.ru>
2024-10-02 13:43:26 +0300

    plotter update

--- utils/plotter.py
+++ utils/plotter.py
@@ -1,30 +1,72 @@
-import torch
-from matplotlib import pyplot as plt
+import cv2
+import numpy as np
 from utils import box
 
 
 class Plotter:
-    def __init__(self, rows=2, columns=4, threshold=0.8, labels=None):
-        self.rows = rows
-        self.columns = columns
+    def __init__(self, threshold=0.8, labels=None, interval=200, wight=4):
         self.threshold = threshold
         self.labels = labels
         self.colors = ["g", "b", "r", "m", "c"]
+        self.interval = interval
+        self.wight = wight
 
-    def display(self, images, predictions, target=None):
-        num_img, _, h, w = images.shape
-        predictions[:, :, 2] *= w
-        predictions[:, :, 4] *= w
-        predictions[:, :, 3] *= h
-        predictions[:, :, 5] *= h
-        fig = plt.figure(figsize=(h, w))
-        images = images.permute(0, 2, 3, 1)
-        ax = []
-        for i in range(min(self.rows * self.columns, num_img)):
-            ax.append(fig.add_subplot(self.rows, self.columns, i + 1))
-            plt.imshow(images[i])
-            if predictions.shape[0] > i:
-                self.show_bboxes(ax[-1], predictions[i])
+    def concatenate_video(self, video: np.ndarray):
+        """Combines a batch of videos into one
+        Args:
+            video (np.ndarray): [ts, b, h, w]
+        Returns:
+            Combines video (np.ndarray): [ts, h, w]
+        """
+        b = video.shape[1]
+        video = np.pad(
+            video,
+            pad_width=(
+                (0, 0),
+                (0, self.wight - b % self.wight),
+                (0, 0),
+                (0, 0),
+            ),
+            constant_values=0,
+        )
+        con_imgs = []
+        for time_stamp in video:
+            arr = [
+                np.concatenate(time_stamp[idx : idx + 4], axis=1)
+                for idx in range(0, b, self.wight)
+            ]
+            con_imgs.append(np.concatenate(arr, axis=0))
+        return np.stack(con_imgs)
+
+    def show_video(self, video: np.ndarray):
+        """Playing video
+        Args:
+            video (np.ndarray): [ts, h, w]
+        Returns:
+            bool: Returns false if "q" is pressed
+        """
+        for time_stamp in video:
+            cv2.imshow("Res", time_stamp)
+            if cv2.waitKey(self.interval) == ord("q"):
+                cv2.destroyWindow("Res")
+                return False
+        return True
+
+    def display(self, images, predictions=None, target=None):
+        """Plays video from tensor
+        Args:
+            images (torch.Tensor): [ts, batch, p, h, w]
+            predictions (_type_, optional): TODO. Defaults to None.
+            target (_type_, optional): TODO. Defaults to None.
+        """
+        ts, b, p, h, w = images.shape
+        plt_images = images.permute(0, 1, 3, 4, 2)
+        grey_imgs = 127 * np.ones((ts, b, h, w), dtype=np.uint8)
+        grey_imgs[plt_images[..., 0] > 0] = 0
+        grey_imgs[plt_images[..., 1] > 0] = 255
+        con_video = self.concatenate_video(grey_imgs)
+        while self.show_video(con_video):
+            pass
 
     def show_bboxes(self, axes, bboxes):
         """Show bounding boxes."""
@@ -39,9 +81,11 @@ class Plotter:
             axes.text(
                 bbox[2],
                 bbox[3],
-                self.labels[int(bbox[0])] + f": {bbox[1]:.2f}" if self.labels is not None else f"{bbox[1]:.2f}",
+                self.labels[int(bbox[0])] + f": {bbox[1]:.2f}"
+                if self.labels is not None
+                else f"{bbox[1]:.2f}",
                 horizontalalignment="left",
                 verticalalignment="bottom",
                 color="r",
-                size="xx-small"
+                size="xx-small",
             )
