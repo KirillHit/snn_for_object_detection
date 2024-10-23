6a241bbd1962d0f61ae784c4d92743f0f5937731
KirillHit <khitushkink@mail.ru>
2024-10-23 13:26:55 +0000

    multi-threaded dataset loading 0.2

--- utils/datasets.py
+++ utils/datasets.py
@@ -14,22 +14,20 @@ import itertools
 class Gen1DataModule(DataModule):
     """Event-Based Dataset to date"""
 
-    def __init__(self, root="./data", batch_size=32, duration=1000, num_workers=4):
+    def __init__(self, root="./data", batch_size=32, num_steps=16, num_workers=4):
         super().__init__(root, num_workers=num_workers, batch_size=batch_size)
-        self.duration = duration
+        self.num_steps = num_steps
 
     def read_data(self, split: str):
         # Data dir: ./data/gen1/<test, train, val>
         data_dir = os.path.join(self._root, "gen1", split)
-        if not os.path.isdir(data_dir):
-            raise RuntimeError(f'Directory "{data_dir}" does not exist!')
         # Get files name
         gt_files = glob.glob(data_dir + "/*_bbox.npy")
         data_files = [p.replace("_bbox.npy", "_td.dat") for p in gt_files]
 
         if not data_files or not gt_files or len(data_files) != len(gt_files):
-            raise RuntimeError(
-                f"Directory '{data_dir}' does not contain data or data is invalid! I'm expecting: "
+            tqdm.write(
+                f"[WARN]: Directory '{data_dir}' does not contain data or data is invalid! I'm expecting: "
                 f"./data/{data_dir}/*_bbox.npy (and *_td.dat). "
                 "The dataset can be downloaded from this link: "
                 "https://www.prophesee.ai/2020/01/24/prophesee-gen1-automotive-detection-dataset/"
@@ -45,10 +43,10 @@ class Gen1DataModule(DataModule):
             case "val":
                 self._val_dataset = dataset
             case _:
-                raise ValueError(f'The split parameter cannot be "{split}"!')
+                raise ValueError(f'[ERROR]: The split parameter cannot be "{split}"!')
 
         print(
-            str(len(data_files)) + " " + split + " samples loaded from Gen1 dataset..."
+            "[INFO]: " + str(len(data_files)) + " " + split + " samples loaded from Gen1 dataset..."
         )
 
     def create_dataset(
@@ -68,26 +66,30 @@ class Gen1Fixed(Gen1DataModule):
         self,
         root="./data",
         batch_size=32,
-        duration=1000,
+        num_steps=16,
         time_step=100,
         num_load_file=50,
         num_workers=4,
     ):
-        super().__init__(root, batch_size, duration, num_workers)
+        super().__init__(root, batch_size, num_steps, num_workers)
         self.time_step, self.num_load_file = time_step, num_load_file
 
     def create_dataset(
         self, gt_files: list[str], data_files: list[str]
     ) -> IterableDataset:
         return Gen1FixedDataset(
-            gt_files, data_files, self.time_step, self.duration, self.num_load_file
+            gt_files,
+            data_files,
+            self.time_step,
+            self.num_steps,
+            self.num_load_file,
         )
 
 
 class Gen1FixedDataset(IterableDataset):
     """A customized dataset to load the banana detection dataset."""
 
-    record_time = 60000  # ms
+    record_time = 60000000  # ns
     width: int = 304
     height: int = 240
 
@@ -96,7 +98,7 @@ class Gen1FixedDataset(IterableDataset):
         gt_files: list[str],
         data_files: list[str],
         time_step: int,
-        duration: int,
+        num_steps: int,
         num_load_file: int,
     ):
         """
@@ -104,30 +106,35 @@ class Gen1FixedDataset(IterableDataset):
             gt_files (list[str]): List of ground truth file paths
             data_files (list[str]): List of data file paths
             time_step (int): Duration for one frame (ms)
-            duration (int): Sample duration (ms)
+            num_steps (int): Number of time steps
             num_load_file (int): Number of examples loaded at a time
         """
         assert num_load_file > 0, "The number of loaded files must be more than zero"
 
         self.gt_files, self.data_files = gt_files, data_files
-        self.duration, self.time_step = duration, time_step
+        self.num_steps, self.time_step = num_steps, time_step
         self.num_load_file = num_load_file
-        self.record_steps = self.record_time // self.duration
-        self.sequence_len = ((self.duration - 1) // self.time_step) + 1
+        self.time_step_ns = self.time_step * 1000
+        self.duration_ns = self.time_step_ns * self.num_steps
+        self.record_steps = self.record_time // self.duration_ns
 
     def __iter__(self) -> Iterator[tuple[torch.Tensor, torch.Tensor]]:
         return iter(self.samples_generator())
 
     def __len__(self) -> int:
-        return self.num_load_file * self.record_steps
+        return len(self.gt_files) * self.record_steps
 
     def samples_generator(
         self,
     ) -> Generator[tuple[torch.Tensor, torch.Tensor], None, None]:
+        if not self.gt_files:
+            raise RuntimeError("Attempt to access unloaded part of dataset")
         file_loader = self.load_generator()
+        shuffle_idx = list(range(self.num_load_file * self.record_steps))
+        shuffle(shuffle_idx)
         while True:
             gt_boxes_list, events_loaders = next(file_loader)
-            for idx in range(len(self)):
+            for idx in shuffle_idx:
                 data_idx = idx % self.num_load_file
                 yield self.parse_data(gt_boxes_list[data_idx], events_loaders[data_idx])
 
@@ -139,20 +146,22 @@ class Gen1FixedDataset(IterableDataset):
         ############ Features preparing ############
         # Return features format (ts, c [0-negative, 1-positive], h, w)
         features = torch.zeros(
-            [self.sequence_len, 2, self.height, self.width],
+            [self.num_steps, 2, self.height, self.width],
             dtype=torch.float32,
         )
         # Events format ('t' [us], 'x', 'y', 'p' [1-positive/0-negative])
         if events_loader.done:
             events_loader.reset()
-        events = events_loader.load_delta_t(self.duration * 1000)
-        time_stamps = events[:]["t"] // (self.time_step * 1000)
+        start_time = events_loader.current_time // (self.time_step_ns)
+        end_time = start_time + self.num_steps
+        events = events_loader.load_delta_t(self.duration_ns)
+        time_stamps = (events[:]["t"] // (self.time_step_ns)) - start_time
 
         if not time_stamps.size:
             return (features, gt_boxes[0:0])
 
         features[
-            time_stamps[:] - time_stamps[0],
+            time_stamps[:],
             events[:]["p"].astype(np.uint32),
             events[:]["y"].astype(np.uint32),
             events[:]["x"].astype(np.uint32),
@@ -161,10 +170,8 @@ class Gen1FixedDataset(IterableDataset):
         ############ Labels preparing ############
         # Return labels format (ts, class id (0 car, 1 person), xlu, ylu, xrd, yrd)
         # Box update frequency 1-4 Hz
-        labels = gt_boxes[
-            (gt_boxes[:, 0] >= time_stamps[0]) & (gt_boxes[:, 0] <= time_stamps[-1])
-        ]
-        labels[:, 0] -= time_stamps[0]
+        labels = gt_boxes[(gt_boxes[:, 0] >= start_time) & (gt_boxes[:, 0] < end_time)]
+        labels[:, 0] -= start_time
 
         return (features, labels)
 
@@ -173,13 +180,16 @@ class Gen1FixedDataset(IterableDataset):
     ) -> Generator[tuple[list[torch.Tensor], list[PSEELoader]], None, None]:
         """Loads num_load_file new samples from the list.
         This is necessary when working with a large number of files."""
-        shuffle_file_idx = list(range(len(self.gt_files)))
+        
+        worker_info = get_worker_info()
+        id = worker_info.id
+        num_workers = worker_info.num_workers
+        per_worker = len(self.gt_files) // num_workers
+        
+        # Shuffle loaded files
+        shuffle_file_idx = list(range(per_worker * id, per_worker * (id + 1)))
         shuffle(shuffle_file_idx)
         idx_file_iter = itertools.cycle(iter(shuffle_file_idx))
-
-        """ TODO 
-        worker_info = get_worker_info()
-        print(worker_info.id) """
 
         while True:
             labels: list[np.ndarray] = []
@@ -189,7 +199,6 @@ class Gen1FixedDataset(IterableDataset):
                     break
                 labels.append(np.load(self.gt_files[file_idx]))
                 loaders.append(PSEELoader(self.data_files[file_idx]))
-            tqdm.write(f"[INFO]: {self.num_load_file} files uploaded")
             yield self.labels_prepare(labels), loaders
 
     def labels_prepare(self, labels: list[np.ndarray]) -> list[torch.Tensor]:
@@ -206,7 +215,7 @@ class Gen1FixedDataset(IterableDataset):
             torch.from_numpy(
                 np.array(
                     [
-                        gt_boxes[:]["ts"] // (self.time_step * 1000),
+                        gt_boxes[:]["ts"] // (self.time_step_ns),
                         gt_boxes[:]["class_id"],
                         gt_boxes[:]["x"] / self.width,
                         gt_boxes[:]["y"] / self.height,
@@ -227,10 +236,10 @@ class Gen1Adaptive(Gen1DataModule):
         self,
         root="./data",
         batch_size=32,
-        duration=1000,
+        num_steps=16,
         event_step=100,
     ):
-        super().__init__(root, batch_size, duration)
+        super().__init__(root, batch_size, num_steps)
         self.event_step = event_step
 
     def create_dataset(
