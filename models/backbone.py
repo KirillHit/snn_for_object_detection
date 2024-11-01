b3814f3f4fa5a126ec555304b4c3a5e76f5202de
KirillHit <khitushkink@mail.ru>
2024-11-01 16:55:23 +0300

    fix

--- models/backbone.py
+++ models/backbone.py
@@ -1,17 +1,29 @@
 import torch
 from torch import nn
-from typing import Any, cast, Dict, List, Optional, Union
+from typing import cast, Dict, List, Union
 import norse.torch as snn
 
 
-class Backbone(nn.Module):
-    net: snn.SequentialState
-
+class VGGBackbone(nn.Module):
+    # fmt: off
+    cfgs: Dict[str, List[Union[str, int]]] = {
+        11: [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+        13: [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+        16: [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
+        19: [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
+    }
+    # fmt: on
     def __init__(
         self,
-        init_weights: bool = True,
+        layers: int,
+        in_channels=2,
+        init_weights=True,
+        batch_norm=False,
     ) -> None:
         super().__init__()
+
+        self.net = self.make_layers(self.cfgs[layers], batch_norm, in_channels)
+
         if init_weights:
             for m in self.modules():
                 if isinstance(m, nn.Conv2d):
@@ -26,30 +38,6 @@ class Backbone(nn.Module):
                 elif isinstance(m, nn.Linear):
                     nn.init.normal_(m.weight, 0, 0.01)
                     nn.init.constant_(m.bias, 0)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        spikes, state = self.net(x)
-        return spikes
-
-class VggBased(Backbone):
-    # fmt: off
-    cfgs: Dict[str, List[Union[str, int]]] = {
-        "11": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
-        "13": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
-        "16": [64, 64, "M", 128, 128, "M", 256, 256, 256, "M", 512, 512, 512, "M", 512, 512, 512, "M"],
-        "19": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"],
-    }
-    # fmt: on
-    def __init__(
-        self,
-        model: str,
-        in_channels=2,
-        init_weights=True,
-        batch_norm=False,
-    ) -> None:
-        super().__init__(init_weights)
-
-        self.net = self.make_layers(model, batch_norm, in_channels)
 
     def make_layers(
         cfg: List[Union[str, int]], batch_norm: bool, in_channels: int
@@ -71,3 +59,7 @@ class VggBased(Backbone):
                     layers += [conv2d, snn.LIF()]
                 in_channels = v
         return snn.SequentialState(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        spikes, state = self.net(x)
+        return spikes
